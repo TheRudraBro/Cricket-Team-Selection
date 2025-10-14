2e85b19bf50f6c8263d73727e75061fc5d8d1c31
TheRudraBro <rudramraj22@gmail.com>
2025-10-14 23:56:34 +0600

    Updated

--- src/components/SelectedCard/SelectedCard.jsx
+++ src/components/SelectedCard/SelectedCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const SelectedCard = (player) => {
+const SelectedCard = ({player}) => {
     console.log(player);
     return (
-         <div className='border-2 border-gray-300 rounded-2xl flex justify-between items-center p-4'>
+         <div className='border-2 border-gray-300 rounded-2xl flex justify-between items-center p-4 mt-4'>
 <div className='flex items-center gap-4'>
     <img src={player.player_image} className='h-[60px] w-[60px] rounded-xl' alt="" />
     <div>
         <h1 className='font-semibold text-xl'>{player.player_name}</h1>
-        <p className=''>{player.player_role}</p>
+        <p className=''>{player.playing_role}</p>
     </div>
 </div>
 <div>
