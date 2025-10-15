1cef9c32dfa889a54343780362eb34bb8ea7cb8b
TheRudraBro <rudramraj22@gmail.com>
2025-10-15 17:01:20 +0600

    Updted

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,56 @@ function App() {
 
 <ToastContainer></ToastContainer>
 
+<main className='max-w-[1200px] mx-auto text-center p-10 mt-10 border-2 border-gray-300 rounded-xl mb-10'>
+
+
+<h1 className='font-bold'>Subscribe to our Newsletter</h1>
+<p className='mt-4'>Get the latest updates and news right in your inbox!</p>
+<div className='mt-4'>
+  <div>
+    <label className="input validator">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+    </g>
+  </svg>
+  <input type="email" placeholder="mail@site.com" required />
+</label>
+<div className="validator-hint hidden">Enter valid email address</div>
+  </div>
+  <div className='mt-4'>
+    <button className='bg-gray-800 text-white p-2 rounded-lg'>Send Email</button>
+  </div>
+</div>
+
+
+
+
+
+
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <footer className='bg-gray-800 '>
 
